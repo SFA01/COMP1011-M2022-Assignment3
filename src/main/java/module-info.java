6d1daa88d943b4a0725ebb80e2a0c6283d89eef6
SFA01 <sarafranzinoandrade@hotmail.com>
2022-08-21 16:40:19 -0400

    Own API Key added

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,10 @@
 module ca.georgiancollege.comp1011m2022assignment3 {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
-    opens ca.georgiancollege.comp1011m2022assignment3 to javafx.fxml;
+    requires java.sql;
+    requires com.google.gson;
+    requires java.net.http;
+
+    opens ca.georgiancollege.comp1011m2022assignment3 to javafx.fxml, com.google.gson;
     exports ca.georgiancollege.comp1011m2022assignment3;
 }
